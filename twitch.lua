0d3890117881e9d6c55be57fc54ccdaa7e3dd656
Edgars Baduns <edgars.baduns@gmail.com>
2014-01-03 21:29:01 +0200

    No need to parse title for initial url

--- twitch.lua
+++ twitch.lua
@@ -18,10 +18,6 @@ function parse()
         if string.match(line, '<meta property="og:video"') then
             realid = string.match(line, 'videoId=(.%d+)')
         end
-
-        if string.match(line, '<meta property="og:title"') then
-            title = string.match(line, 'content="([^\"]+)"')
-        end
     end
 
     return { { path='http://api.twitch.tv/api/videos/' .. realid, title = title } }
