1bf33f65c7b21cd5a4869c26700b5a77b9110343
Edgars Baduns <edgars.baduns@gmail.com>
2014-01-03 21:45:53 +0200

    Use part start time as title

--- twitchapi.lua
+++ twitchapi.lua
@@ -1,3 +1,7 @@
+function secToHMS(s)
+    return string.format("%.2d:%.2d:%.2d", s / (60 * 60), s / 60 % 60, s % 60)
+end
+
 function probe()
     if vlc.access ~= 'http' and vlc.access ~= 'https' then
         return false
@@ -9,8 +13,10 @@ end
 function parse()
     local str = vlc.readline()
     local parts = {}
+    local currtime = 0
     for url, duration in string.gmatch(str, '"url":"([^\"]+)","length":(%d+)') do
-        table.insert(parts, {path = url, duration = duration })
+        table.insert(parts, {path = url, duration = duration, title = secToHMS(currtime) })
+        currtime = currtime + duration
     end
 
     return parts
